43bac5e351f6fec2abd6a7d264042ea3680e277b
[Github <[Github>
2023-01-03 20:20:24 +0900

    todo 리스트 target 이용하여 삭제기능 추가

--- js/todo.js
+++ js/todo.js
@@ -6,10 +6,19 @@ const todoList = document.getElementById("todo-list");
 
 // let todos = [];
 
+function deleteTodo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function paintTodo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
+  const button = document.createElement("button");
+  button.innerText = "X";
+  button.addEventListener("click", deleteTodo);
   li.appendChild(span);
+  li.appendChild(button);
   span.innerText = newTodo;
   todoList.appendChild(li);
 }
