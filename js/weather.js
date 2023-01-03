79e85c27cd81f06cd62658af29b909fbe83ced69
[Github <[Github>
2023-01-04 00:12:53 +0900

    API 이용하여 도시, 날씨 추가

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = "5c1ccac7cb7d72fada33a9964dbad2ed";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const city = document.querySelector("#waether span:first-child");
+      const weather = document.querySelector("#waether span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No waether for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
