3845488e427295af8ab6eb8ff2bdc0c73e80422b
[Github <[Github>
2023-01-03 21:22:47 +0900

    LocalStorage에 todo 저장하기

--- js/todo.js
+++ js/todo.js
@@ -2,9 +2,13 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = todoForm.querySelector("#todo-form input");
 const todoList = document.getElementById("todo-list");
 
-// const TODOS_KEY = "todos";
+const TODOS_KEY = "todos";
 
-// let todos = [];
+let todos = [];
+
+function saveTodo() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
+}
 
 function deleteTodo(event) {
   const li = event.target.parentElement;
@@ -27,7 +31,17 @@ function handleTodoSubmit(event) {
   event.preventDefault();
   const newTodo = todoInput.value;
   todoInput.value = "";
+  todos.push(newTodo);
   paintTodo(newTodo);
+  saveTodo();
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
+
+const savedTodos = localStorage.getItem(TODOS_KEY);
+
+if (savedTodos !== null) {
+  const parsedTodos = JSON.parse(savedTodos);
+  todos = parsedTodos;
+  parsedTodos.forEach(paintTodo);
+}
